7d550a79a4da4fbdfdc28f0fe10636c71ff5404d
kanghyunwi <kanghyun.wi@icloud.com>
2023-11-22 14:10:31 +0800

    끝

--- Assets/Mini First Person Controller/Scripts/Monster.cs
+++ Assets/Mini First Person Controller/Scripts/Monster.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+    NavMeshAgent nav;
+    GameObject target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (nav.destination != target.transform.position)
+        {
+            nav.SetDestination(target.transform.position);
+        }
+        else
+        {
+            nav.SetDestination(transform.position);
+        }
     }
 }
