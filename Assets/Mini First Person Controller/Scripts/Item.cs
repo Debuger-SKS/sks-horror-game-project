1f15e60d15d653f14c1672ca46525804ac4d22a5
kanghyunwi <kanghyun.wi@icloud.com>
2023-11-29 14:46:58 +0800

    2.1

--- Assets/Mini First Person Controller/Scripts/Item.cs
+++ Assets/Mini First Person Controller/Scripts/Item.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    GameObject nearObject; void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Item")
-        {
-            nearObject = other.gameObject;
-        }
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Item")
-        {
-            nearObject = null;
-        }
-    }
-
     // Start is called before the first frame update
     void Start()
     {
